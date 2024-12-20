5a1b51f017ef307b465947a5401812d23a593ff2
Michael <michaelritchey811@gmail.com>
2024-12-21 04:18:26 +0900

    6th

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: 'Anastasije Pavlovic',
+          to_name: 'John Carlo Cruz',
           from_email: form.email,
           subject: form.subject,
           message: form.message,
